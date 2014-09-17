56797de6a41cc58198e661341b4b59eb35300dd7
Radu Cigmaian <radu.cigmaian@gmail.com>
2014-09-17 20:42:30 +0300

    role based validatio added

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -7,15 +7,15 @@ var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/'/*, auth.hasRole('admin')*/, controller.index);
+router.get('/', auth.hasRole('admin'), controller.index);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.post('/', controller.create);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/:id/categories', /*auth.isAuthenticated(),*/controller.updateCategories);
-router.get('/:id/categories', /*auth.isAuthenticated(),*/controller.getCategories);
-router.post('/:id/frequencies', /*auth.isAuthenticated(),*/controller.updateFrequencies);
-router.get('/:id/frequencies', /*auth.isAuthenticated(),*/controller.getFrequencies);
+router.post('/:id/categories', auth.isAuthenticated(), controller.updateCategories);
+router.get('/:id/categories', auth.isAuthenticated(),controller.getCategories);
+router.post('/:id/frequencies', auth.isAuthenticated(), controller.updateFrequencies);
+router.get('/:id/frequencies', auth.isAuthenticated(), controller.getFrequencies);
 
 module.exports = router;
